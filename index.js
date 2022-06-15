f42a9cc45a4336135e983ea29e2375a9882cfc9d
unknown <varunmalpani.vm01@gmail.com>
2022-06-15 20:19:16 +0530

    8th commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const static_path = path.join(__dirname, "./public");
 app.use(express.static(static_path));
